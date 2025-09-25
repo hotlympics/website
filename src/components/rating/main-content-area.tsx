81f6e5b85afbf707c298f1f3f270f8a87efebe89
Tarquinen <dannysmo@gmail.com>
2025-09-25 16:24:15 -0400

    reeeeeeee

--- src/components/rating/main-content-area.tsx
+++ src/components/rating/main-content-area.tsx
@@ -7,10 +7,10 @@ interface MainContentAreaProps {
 export const MainContentArea = ({ children }: MainContentAreaProps) => {
     return (
         <div
-            className="flex min-h-[100dvh] w-full flex-col items-center justify-center"
+            className="flex min-h-[100dvh] w-full flex-col items-center justify-center pb-24 sm:pb-20"
             style={{
                 paddingTop: "1rem",
-                paddingBottom: "calc(6rem + env(safe-area-inset-bottom))",
+                paddingBottom: "calc(7rem + env(safe-area-inset-bottom))",
             }}
         >
             {children}
