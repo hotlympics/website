50fe284fd4b31fb9d9e4eb50a3bee490302ae826
jorgenwh <jorgenwhen@gmail.com>
2025-10-07 20:01:09 +0200

    fix padding at the bottom of leaderboards tab

--- src/pages/leaderboard-page.tsx
+++ src/pages/leaderboard-page.tsx
@@ -79,7 +79,7 @@ const LeaderboardPage = () => {
     }
 
     return (
-        <div className="min-h-screen bg-gray-900 px-4 py-8">
+        <div className="min-h-screen bg-gray-900 px-4 py-8 pb-28">
             <div className="mx-auto max-w-6xl">
                 {/* Header */}
                 <LeaderboardHeader
